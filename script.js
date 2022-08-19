df85b643cd6ecf9635dc375f0b33017853b1c694
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-08-18 23:12:57 -0300

    darkMode option

---
+++ script.js
@@ -0,0 +1,25 @@
+const cat = document.querySelector('.extra');
+const catText = document.querySelector('.cat-p')
+const leftEar = document.querySelector('.ear-left')
+const rightEar = document.querySelector('.ear-right')
+const navBar = document.querySelector('.nav-bar')
+
+document.body.style.backgroundColor = '#FEFAF6'
+function changeMode() {
+
+  navBar.classList.toggle('darkMode')
+  cat.classList.toggle('lightMode')
+  leftEar.classList.toggle('darkMode')
+  rightEar.classList.toggle('darkMode')
+  if ( document.body.style.backgroundColor === 'rgb(254, 250, 246)'){
+    document.body.style.backgroundColor = '#161616'
+    document.body.style.color = 'white'
+    catText.innerHTML = 'Clique em mim para Light Mode'
+  } else if (document.body.style.backgroundColor === 'rgb(22, 22, 22)') {
+    document.body.style.backgroundColor = '#FEFAF6'
+    document.body.style.color = 'black'
+    catText.innerHTML = 'Clique em mim para Dark Mode'
+  }
+}
+
+cat.addEventListener('click', changeMode)
