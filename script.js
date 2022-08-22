d67c477e8cc69ceb97a6429b5d0bfbe6a7dbd297
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-08-22 00:02:12 -0300

    logo

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ const catFace = document.querySelector('.face')
 const pageFooter = document.querySelector('.social-media')
 const btnMobile = document.querySelector('.btn-mobile')
 const hamburguer = document.querySelector('#hamb')
+const logo = navBar.querySelector('img')
 
 
 
 document.body.style.backgroundColor = '#FEFAF6'
 function changeMode() {
-
-  navBar.classList.toggle('darkMode')
   cat.classList.toggle('lightMode')
   leftEar.classList.toggle('darkMode')
   rightEar.classList.toggle('darkMode')
@@ -22,17 +21,13 @@ function changeMode() {
   if ( document.body.style.backgroundColor === 'rgb(254, 250, 246)'){
     document.body.style.backgroundColor = '#161616'
     document.body.style.color = 'white'
-    catText.innerHTML = 'Clique em mim para Light Mode'
+    catText.innerHTML = 'Clique em mim para Light Mode';
     cat.setAttribute('aria-label', 'Botão com imagem de gato com a frase "Clique para mudar para o modo claro"')
-    btnMobile.style.color = 'rgb(254, 250, 246)'
-    hamburguer.style.borderColor = 'rgb(254, 250, 246)'
   } else if (document.body.style.backgroundColor === 'rgb(22, 22, 22)') {
     document.body.style.backgroundColor = '#FEFAF6'
     document.body.style.color = 'black'
     catText.innerHTML = 'Clique em mim para Dark Mode'
     cat.setAttribute('aria-label', 'Botão com imagem de gato com a frase "Clique para mudar para o modo escuro"')
-    btnMobile.style.color = 'rgb(22, 22, 22)'
-    hamburguer.style.borderColor = 'rgb(22, 22, 22)'
   }
 }
 
