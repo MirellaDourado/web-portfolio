45dfb22ccf30897d98095209bab2e7a1fe20bc23
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-08-21 22:59:27 -0300

    menu hamburger + transition

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const navBar = document.querySelector('.nav-bar')
 const catFace = document.querySelector('.face')
 const pageFooter = document.querySelector('.social-media')
 const btnMobile = document.querySelector('.btn-mobile')
+const hamburguer = document.querySelector('#hamb')
 
 
 
@@ -22,10 +23,16 @@ function changeMode() {
     document.body.style.backgroundColor = '#161616'
     document.body.style.color = 'white'
     catText.innerHTML = 'Clique em mim para Light Mode'
+    cat.setAttribute('aria-label', 'Clique para mudar para o modo claro')
+    btnMobile.style.color = 'rgb(254, 250, 246)'
+    hamburguer.style.borderColor = 'rgb(254, 250, 246)'
   } else if (document.body.style.backgroundColor === 'rgb(22, 22, 22)') {
     document.body.style.backgroundColor = '#FEFAF6'
     document.body.style.color = 'black'
     catText.innerHTML = 'Clique em mim para Dark Mode'
+    cat.setAttribute('aria-label', 'Clique para mudar para o modo escuro')
+    btnMobile.style.color = 'rgb(22, 22, 22)'
+    hamburguer.style.borderColor = 'rgb(22, 22, 22)'
   }
 }
 
@@ -34,6 +41,9 @@ cat.addEventListener('click', changeMode)
 const changeMenu = () => {
   const navHamburguer = document.querySelector('.nav-bar');
   navHamburguer.classList.toggle('active');
+  const active = navHamburguer.classList.contains('active')
+  btnMobile.setAttribute('aria-expanded', active)
 }
+  
 
 btnMobile.addEventListener('click', changeMenu);
