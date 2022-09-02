22cd880732438b6683227b8ad92e364ccb99b261
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-09-01 23:36:49 -0300

    retirada da função do gato - script.js

--- script.js
+++ script.js
@@ -8,30 +8,6 @@ const pageFooter = document.querySelector('.social-media')
 const btnMobile = document.querySelector('.btn-mobile')
 const hamburguer = document.querySelector('#hamb')
 
-
-
-document.body.style.backgroundColor = '#FEFAF6'
-function changeMode() {
-  cat.classList.toggle('lightMode')
-  leftEar.classList.toggle('darkMode')
-  rightEar.classList.toggle('darkMode')
-  catFace.classList.toggle('darkMode')
-  pageFooter.classList.toggle('darkMode')
-  if ( document.body.style.backgroundColor === 'rgb(254, 250, 246)'){
-    document.body.style.backgroundColor = '#161616'
-    document.body.style.color = 'white'
-    catText.innerHTML = 'Clique em mim para Light Mode';
-    cat.setAttribute('aria-label', 'Botão com imagem de gato com a frase "Clique para mudar para o modo claro"')
-  } else if (document.body.style.backgroundColor === 'rgb(22, 22, 22)') {
-    document.body.style.backgroundColor = '#FEFAF6'
-    document.body.style.color = 'black'
-    catText.innerHTML = 'Clique em mim para Dark Mode'
-    cat.setAttribute('aria-label', 'Botão com imagem de gato com a frase "Clique para mudar para o modo escuro"')
-  }
-}
-
-cat.addEventListener('click', changeMode)
-
 const changeMenu = () => {
   const navHamburguer = document.querySelector('.nav-bar');
   navHamburguer.classList.toggle('active');
